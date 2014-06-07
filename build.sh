6e9bf35086d60966325694e0b012da17083b7dc4
Frank Bennett <biercenator@gmail.com>
2014-06-07 18:18:27 +0900

    Trim git repo data dir

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ find . -name '*~' -exec rm {} \;
 find . -name '*.sh' -prune -o \
        -name '*.tmpl' -prune -o \
        -name 'bluebook-signals-for-zotero.*' -prune -o \
+       -name '.git' -prune -o \
        -name '*~' -prune -o \
        -wholename './about.xul' -prune -o \
        -print0 | xargs -0 zip bluebook-signals-for-zotero.xpi
