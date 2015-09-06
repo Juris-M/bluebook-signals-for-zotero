6c95687fa774daf9f5c65e9a846774a06aa03cc3
fbennett <biercenator@gmail.com>
2015-09-06 17:15:57 +0900

    Fix formatting, drop unneeded trace comments

--- bootstrap.js
+++ bootstrap.js
@@ -7,41 +7,41 @@ Cu.import("resource://gre/modules/Services.jsm");
  * From https://developer.mozilla.org/en-US/Add-ons/How_to_convert_an_overlay_extension_to_restartless
  */
 function getGenericPref(branch,prefName) {
-    switch (branch.getPrefType(prefName)) {
-    default:
-    case 0:   return undefined;                      // PREF_INVALID
-    case 32:  return getUCharPref(prefName,branch);  // PREF_STRING
-    case 64:  return branch.getIntPref(prefName);    // PREF_INT
-    case 128: return branch.getBoolPref(prefName);   // PREF_BOOL
-    }
+	switch (branch.getPrefType(prefName)) {
+	default:
+	case 0:   return undefined;                      // PREF_INVALID
+	case 32:  return getUCharPref(prefName,branch);  // PREF_STRING
+	case 64:  return branch.getIntPref(prefName);    // PREF_INT
+	case 128: return branch.getBoolPref(prefName);   // PREF_BOOL
+	}
 }
 function setGenericPref(branch,prefName,prefValue) {
-    switch (typeof prefValue) {
-    case "string":
-        setUCharPref(prefName,prefValue,branch);
-        return;
-    case "number":
-        branch.setIntPref(prefName,prefValue);
-        return;
-    case "boolean":
-        branch.setBoolPref(prefName,prefValue);
-        return;
-    }
+	switch (typeof prefValue) {
+	case "string":
+		setUCharPref(prefName,prefValue,branch);
+		return;
+	case "number":
+		branch.setIntPref(prefName,prefValue);
+		return;
+	case "boolean":
+		branch.setBoolPref(prefName,prefValue);
+		return;
+	}
 }
 function setDefaultPref(prefName,prefValue) {
-    var defaultBranch = Services.prefs.getDefaultBranch(null);
-    setGenericPref(defaultBranch,prefName,prefValue);
+	var defaultBranch = Services.prefs.getDefaultBranch(null);
+	setGenericPref(defaultBranch,prefName,prefValue);
 }
 function getUCharPref(prefName,branch) { // Unicode getCharPref
-    branch = branch ? branch : Services.prefs;
-    return branch.getComplexValue(prefName, Components.interfaces.nsISupportsString).data;
+	branch = branch ? branch : Services.prefs;
+	return branch.getComplexValue(prefName, Components.interfaces.nsISupportsString).data;
 }
 function setUCharPref(prefName,text,branch) { // Unicode setCharPref
-    var string = Components.classes["@mozilla.org/supports-string;1"]
-        .createInstance(Components.interfaces.nsISupportsString);
-    string.data = text;
-    branch = branch ? branch : Services.prefs;
-    branch.setComplexValue(prefName, Components.interfaces.nsISupportsString, string);
+	var string = Components.classes["@mozilla.org/supports-string;1"]
+		.createInstance(Components.interfaces.nsISupportsString);
+	string.data = text;
+	branch = branch ? branch : Services.prefs;
+	branch.setComplexValue(prefName, Components.interfaces.nsISupportsString, string);
 }
 
 /*
@@ -49,106 +49,102 @@ function setUCharPref(prefName,text,branch) { // Unicode setCharPref
  */
 var ObservePopups = function () {};
 ObservePopups.prototype = {
-    observe: function(subject, topic, data) {
-        var wnd = subject.QueryInterface(Components.interfaces.nsIDOMWindow);
-        wnd.addEventListener("DOMContentLoaded", function (event) {
-            // XXX Check that this document qualifies, and return immediately if not.
-            var doc = event.target;
-            var winID = doc.documentElement.getAttribute('windowtype');
-            if (winID != 'zotero:item-selector' && winID != 'zotero:quick-item-selector') return;
+	observe: function(subject, topic, data) {
+		var wnd = subject.QueryInterface(Components.interfaces.nsIDOMWindow);
+		wnd.addEventListener("DOMContentLoaded", function (event) {
+			// XXX Check that this document qualifies, and return immediately if not.
+			var doc = event.target;
+			var winID = doc.documentElement.getAttribute('windowtype');
+			if (winID != 'zotero:item-selector' && winID != 'zotero:quick-item-selector') return;
 
-            dump("XXX Signals are go!\n");
+			var popupSet = doc.getElementsByTagName('popupset')[0];
+			if (popupSet) return;
 
-            var popupSet = doc.getElementsByTagName('popupset')[0];
-            if (popupSet) return;
+			// (1) popupset node
+			popupSet = doc.createElement('popupset');
+			var lastChild = doc.childNodes[doc.childNodes.length-1];
+			var firstGrandChild = lastChild.childNodes[0];
+			lastChild.insertBefore(popupSet,firstGrandChild);
+			
+			// (2) Keyboard handler
+			function showMenu (event) {
+				if (event.keyCode==83 && event.ctrlKey) {
+					event.preventDefault();
+					// Open popup menu
+					var signalsPopup = doc.getElementById('signals-popup');
+					signalsPopup.openPopup(event.target, "after_start", 0, 0, false, false);
+				}
+			}
 
-            dump("XXX Signals are GO!\n");
+			// (3) Signal insert event handler
+			wnd.setSignal = function(node, event) {
+				var signalElem = event.target;
+				var signal = signalElem.getAttribute('value');
+				var fieldElem = node.parentNode.anchorNode;
+				var selectionStart = fieldElem.selectionStart;
+				var selectionEnd = fieldElem.selectionEnd;
+				var val = fieldElem.value;
+				var start = val.slice(0,selectionStart);
+				var end = val.slice(selectionEnd);
+				// Add spacing
+				if (start.length) {
+					signal = ' ' + signal
+				}
+				signal = signal + ' ';
+				// Normalize spaces
+				val = (start + signal + end).replace(/\s+/g, ' ');
+				fieldElem.value = val;
+			}
 
-            // (1) popupset node
-            popupSet = doc.createElement('popupset');
-            var lastChild = doc.childNodes[doc.childNodes.length-1];
-            var firstGrandChild = lastChild.childNodes[0];
-            lastChild.insertBefore(popupSet,firstGrandChild);
-            
-            // (2) Keyboard handler
-            function showMenu (event) {
-                if (event.keyCode==83 && event.ctrlKey) {
-                    event.preventDefault();
-                    // Open popup menu
-                    var signalsPopup = doc.getElementById('signals-popup');
-                    signalsPopup.openPopup(event.target, "after_start", 0, 0, false, false);
-                }
-            }
+			// (4) Utility function to apply italics
+			function italicize (str) {
+				if (str.slice(-1) === ',') {
+					str = '<i>' + str.slice(0,-1) + '</i>' + str.slice(-1);
+				} else {
+					str = '<i>' + str + '</i>';
+				}
+				return str;
+			}
 
-            // (3) Signal insert event handler
-            wnd.setSignal = function(node, event) {
-                var signalElem = event.target;
-                var signal = signalElem.getAttribute('value');
-                var fieldElem = node.parentNode.anchorNode;
-                var selectionStart = fieldElem.selectionStart;
-                var selectionEnd = fieldElem.selectionEnd;
-                var val = fieldElem.value;
-                var start = val.slice(0,selectionStart);
-                var end = val.slice(selectionEnd);
-                // Add spacing
-                if (start.length) {
-                    signal = ' ' + signal
-                }
-                signal = signal + ' ';
-                // Normalize spaces
-                val = (start + signal + end).replace(/\s+/g, ' ');
-                fieldElem.value = val;
-            }
+			// (4) Junk to build the menu
+			var signalsPopupElem = doc.createElement('menupopup');
+			signalsPopupElem.setAttribute('id','signals-popup');
+			var branch = Services.prefs.getBranch('extensions.bluebook-signals.');
+			var signals = JSON.parse(getGenericPref(branch, 'signals'));
+			for (var i=0,ilen=signals.length;i<ilen;i+=1) {
+				var menuItemElem = doc.createElement('menuitem');
+				var firstChar = signals[i].slice(0,1);
+				var remainderChars = signals[i].slice(1);
+				var val = (firstChar.toUpperCase() + remainderChars);
+				menuItemElem.setAttribute('value',italicize(val));
+				menuItemElem.setAttribute('label',val);
+				menuItemElem.setAttribute('oncommand','setSignal(this, event)')
+				signalsPopupElem.appendChild(menuItemElem);
+			}
+			for (var i=0,ilen=signals.length;i<ilen;i+=1) {
+				var menuItemElem = doc.createElement('menuitem');
+				menuItemElem.setAttribute('value',italicize(signals[i]));
+				menuItemElem.setAttribute('label',signals[i]);
+				menuItemElem.setAttribute('oncommand','setSignal(this, event)')
+				signalsPopupElem.appendChild(menuItemElem);
+			}
+			popupSet.appendChild(signalsPopupElem);
 
-            // (4) Utility function to apply italics
-            function italicize (str) {
-                if (str.slice(-1) === ',') {
-                    str = '<i>' + str.slice(0,-1) + '</i>' + str.slice(-1);
-                } else {
-                    str = '<i>' + str + '</i>';
-                }
-                return str;
-            }
-
-            // (4) Junk to build the menu
-            var signalsPopupElem = doc.createElement('menupopup');
-            signalsPopupElem.setAttribute('id','signals-popup');
-            var branch = Services.prefs.getBranch('extensions.bluebook-signals.');
-            var signals = JSON.parse(getGenericPref(branch, 'signals'));
-            for (var i=0,ilen=signals.length;i<ilen;i+=1) {
-                var menuItemElem = doc.createElement('menuitem');
-                var firstChar = signals[i].slice(0,1);
-                var remainderChars = signals[i].slice(1);
-                var val = (firstChar.toUpperCase() + remainderChars);
-                menuItemElem.setAttribute('value',italicize(val));
-                menuItemElem.setAttribute('label',val);
-                menuItemElem.setAttribute('oncommand','setSignal(this, event)')
-                signalsPopupElem.appendChild(menuItemElem);
-            }
-            for (var i=0,ilen=signals.length;i<ilen;i+=1) {
-                var menuItemElem = doc.createElement('menuitem');
-                menuItemElem.setAttribute('value',italicize(signals[i]));
-                menuItemElem.setAttribute('label',signals[i]);
-                menuItemElem.setAttribute('oncommand','setSignal(this, event)')
-                signalsPopupElem.appendChild(menuItemElem);
-            }
-            popupSet.appendChild(signalsPopupElem);
-
-            // (5) Install the keyboard handler and hope for the best
-            var fieldElem = doc.getElementById('prefix');
-            fieldElem.addEventListener('keydown',showMenu);
-        }, false);
-    },
-    register: function() {
-        var observerService = Components.classes["@mozilla.org/observer-service;1"]
-            .getService(Components.interfaces.nsIObserverService);
-        observerService.addObserver(this, "chrome-document-global-created", false);
-    },
-    unregister: function() {
-        var observerService = Components.classes["@mozilla.org/observer-service;1"]
-            .getService(Components.interfaces.nsIObserverService);
-        observerService.removeObserver(this, "chrome-document-global-created");
-    }
+			// (5) Install the keyboard handler and hope for the best
+			var fieldElem = doc.getElementById('prefix');
+			fieldElem.addEventListener('keydown',showMenu);
+		}, false);
+	},
+	register: function() {
+		var observerService = Components.classes["@mozilla.org/observer-service;1"]
+			.getService(Components.interfaces.nsIObserverService);
+		observerService.addObserver(this, "chrome-document-global-created", false);
+	},
+	unregister: function() {
+		var observerService = Components.classes["@mozilla.org/observer-service;1"]
+			.getService(Components.interfaces.nsIObserverService);
+		observerService.removeObserver(this, "chrome-document-global-created");
+	}
 }
 var observePopups = new ObservePopups();
 
@@ -157,15 +153,15 @@ var observePopups = new ObservePopups();
  * Bootstrap functions
  */
 function startup (data, reason) {
-    // Set up preferences
-    Services.scriptloader.loadSubScript("chrome://bluebook-signals/content/defaultprefs.js",
-                                        {pref:setDefaultPref} );
-    //observeStartup.register();
-    observePopups.register();
+	// Set up preferences
+	Services.scriptloader.loadSubScript("chrome://bluebook-signals/content/defaultprefs.js",
+										{pref:setDefaultPref} );
+	//observeStartup.register();
+	observePopups.register();
 }
 
 function shutdown (data, reason) {
-    observePopups.unregister();
+	observePopups.unregister();
 }
 
 function install (data, reason) {}
