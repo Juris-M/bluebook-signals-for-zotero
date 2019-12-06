2ad39ab6fbb4cb5b4efc34efd1cfada0bc4fac48
Frank Bennett <biercenator@gmail.com>
2019-12-06 11:51:54 +0900

    Update for Jurism fx60 deployment

--- bootstrap.js
+++ bootstrap.js
@@ -10,7 +10,7 @@ function getGenericPref(branch,prefName) {
 	switch (branch.getPrefType(prefName)) {
 	default:
 	case 0:   return undefined;                      // PREF_INVALID
-	case 32:  return getUCharPref(prefName,branch);  // PREF_STRING
+	case 32:  return branch.getCharPref(prefName);  // PREF_STRING
 	case 64:  return branch.getIntPref(prefName);    // PREF_INT
 	case 128: return branch.getBoolPref(prefName);   // PREF_BOOL
 	}
@@ -18,7 +18,7 @@ function getGenericPref(branch,prefName) {
 function setGenericPref(branch,prefName,prefValue) {
 	switch (typeof prefValue) {
 	case "string":
-		setUCharPref(prefName,prefValue,branch);
+		branch.setCharPref(prefName,prefValue);
 		return;
 	case "number":
 		branch.setIntPref(prefName,prefValue);
@@ -31,17 +31,6 @@ function setGenericPref(branch,prefName,prefValue) {
 function setDefaultPref(prefName,prefValue) {
 	var defaultBranch = Services.prefs.getDefaultBranch(null);
 	setGenericPref(defaultBranch,prefName,prefValue);
-}
-function getUCharPref(prefName,branch) { // Unicode getCharPref
-	branch = branch ? branch : Services.prefs;
-	return branch.getComplexValue(prefName, Components.interfaces.nsISupportsString).data;
-}
-function setUCharPref(prefName,text,branch) { // Unicode setCharPref
-	var string = Components.classes["@mozilla.org/supports-string;1"]
-		.createInstance(Components.interfaces.nsISupportsString);
-	string.data = text;
-	branch = branch ? branch : Services.prefs;
-	branch.setComplexValue(prefName, Components.interfaces.nsISupportsString, string);
 }
 
 /*
